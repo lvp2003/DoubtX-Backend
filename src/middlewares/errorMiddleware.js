941f6f12758adca471a9e159799ff893ec4d3c7c
Lakshmi Vara prasad <lakshmivaraprasadmaddiboina@gmail.com>
2025-08-29 09:58:13 +0530

    added the centralized error handling

---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,19 @@
+// 404 Not Found
+
+const notFound = (req, res, next) => {
+  res.status(404).json({ message: `Not found - ${req.originalUrl}` });
+};
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  const status = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(status).json({
+    message: err.message || "Server Error",
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
